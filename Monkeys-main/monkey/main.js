584e4941fe0b9c3f70fa777479395891bbdd2c38
f4briici0 <48632925+f4briici0@users.noreply.github.com>
2023-09-23 01:17:06 -0300

    Add files via upload

---
+++ Monkeys-main/monkey/main.js
@@ -0,0 +1,99 @@
+colors = ["F7567C", "99E1D9", "9CDE9F", "2C497F", "3D2B56", "AD2831", "5EC2B7", "E07A5F", "1F363D", "EC91D8", "613DC1", "BD4F6C", "5DD9C1"]
+frases = ["Macaquinhos todos lindos", "Te amo macaquinho <3", "Você é tão importante pra mim lindinho", "Linduxo", "Tim amu", "Te adoro"]
+
+files = ['127979f2-77b4-4f9f-8e31-9415ad78ce57.webp', '184cee14-2b11-44fe-9038-ff9a840e9258.webp', '1c2ef845-de3b-4952-a74e-b0e63fe8a913.webp', '233d5c50-36b7-4e7a-a0e7-8f411820d0d8.webp', '23cf0e68-52ce-4b38-bbc3-f572703a5007.webp', '280d28f3-045d-4d11-a2e6-49e0c3deabd7.webp', '2c15e3d0-cc15-404a-8035-29332bbed44d.webp', '2d18ac95-3059-4205-ae04-38a6e23a1f44.webp', '2e986e7f-980f-4acf-897e-ededb980fda6.webp', '3c48ec77-74a6-462b-9053-ae6e42f7f03e.webp', '3d909fec-d346-47b4-94ed-5f1dd5c461ca.webp', '45143243-955e-4392-aadd-5bfc2fbda1da.webp', '4848363e-7b86-4edf-8f93-8b0532d0a274.webp', '49c15c7a-bafb-43c7-94a2-e7acbccec0fe.webp', '4a4c26a2-f086-4cd9-8368-33863aca6b9d.webp', '4e218dc6-18d8-4910-b724-934f76555fdc.webp', '4e40b6c9-589b-48b8-8594-574a3105e045.webp', '4e43015f-3263-4392-9636-75352bf3b358.webp', '518d1e73-aa4b-415a-a1d0-a229c67d0787.webp', '569a61e2-943a-47e2-9007-e003ead55494.webp', '5701f168-bba6-4260-b059-ab1db3ca37b7.webp', '57ed5b17-e43c-4946-bc05-dd06acf00ddc.webp', '5baeb75b-59a4-41ae-af77-0cc29c4ac433.webp', '60a47025-3e06-4cc2-8880-25df5b981343.webp', '66f00710-8754-471a-803f-207a02670334.webp', '6c55b835-a089-4b03-aeef-2ee9e4f032b3.webp', '6f1cdaf8-2dd6-4fa7-90f3-3119dbdce82b.webp', '73cc31d1-82a5-4e49-bbdd-bcc899873cc7.webp', '7792dcd7-4ace-4262-a176-10155e4b927c.webp', '786ce26c-5ae4-467f-9a92-4fe15fc6e33a.webp', '7ae780c6-ff69-4412-b7d0-b5f03c241a2c.webp', '7bc6d4fc-179a-4ee4-bd61-9eb2ab545ccd.webp', '7c3b0d00-f451-44e5-a499-b9449839cef3.webp', '7cff48ba-f0ae-4b5f-a7c8-86454416a375.webp', '819f4a28-06ac-4e2e-803f-df51355bebe5.webp', '8359e2e8-e343-4581-ae1c-a72dcd090330.webp', '854e3ca1-5e8f-4b57-9f95-f09ffb7f8fff.webp', '8b4234eb-e6b3-4461-bf9e-c36744a43a79.webp', '99172204-7694-42c5-a812-dacd3552f870.webp', '9c04976f-139a-455e-b69f-ee86b2608ad5.webp', '9d68fc7f-e0e9-4710-afbe-35e707259542.webp', '9e8bbd65-9dc2-428f-b540-c31099ea8e80.webp', '9ec98281-aef2-4bbe-850b-f7e0345fecbd.webp', 'a0f19e89-3f08-4dd3-8cc5-5a1b07c2e3ec.webp', 'a0fa0ae6-aa56-4bf9-8b36-e74e35e32446.webp', 'ae88f060-2738-4234-baed-b895b85ea41a.webp', 'ae97d8c3-79aa-47fb-880a-41836f8c6949.webp', 'b694f5d1-add6-4772-b498-c829d06b5eec.webp', 'b8056e40-6c6a-4c89-aa0d-9d931e3295fa.webp', 'b974b013-3bdb-4bec-8430-bd74c756762c.webp', 'bc7b81e2-f5c5-4a40-91fd-ea4eaa732f02.webp', 'c1f3ec53-e0ab-4065-9fe0-154af85e0153.webp', 'c4f09973-7a1f-4b36-ad52-5616bd7522a8.webp', 'c535d737-70d7-4314-aef2-b118dbc757c6.webp', 'c568415d-85e4-4d64-a530-0868ad38b021.webp', 'c5e285ff-b948-4963-b67c-ccc9a5c4ece0.webp', 'c728377e-a572-4567-b8bf-bda79460a6d2.webp', 'c9933150-df1f-4c7c-adf9-c5bdb03f653c.webp', 'cdd875fd-478f-4129-992e-7a43a0f47b3f.webp', 'ce31978d-2826-442f-bb0f-7be0995f7da9.webp', 'CGDTx4eb.jpg', 'd3bb63b0-5c77-4616-a707-6eac73e113fa.webp', 'd4cec114-b8da-4786-9174-51c2ebfb6e4a.webp', 'dc825bca-8b56-4993-9a12-748d8533851b.webp', 'dd560f1c-2f50-4821-8b92-b3b198fd45e9.webp', 'ee02dff7-3459-4e3e-b1c4-fcd16ea6305c.webp', 'f19a71de-2443-4a9a-a7d0-f5413c5e9e06.webp', 'fab9ff8f-325e-43d7-8361-c37ffc4bec47.webp', 'ffe5e8a6-a2b8-4218-8d3b-55a3763b1691.webp', 'monkey1.webp']
+
+cS = -1;
+cT = -1;
+sorted_color = "2C497F"
+
+options = [true, false, false, true, false, false]
+
+function monkey(){
+	document.getElementById('monkey_img').src = "imgs/" + files[Math.floor(Math.random() * (files.length))];
+}
+
+window.onload = monkey();
+
+function main(){
+
+	monkey()
+
+	if (options[0] == true){
+		sorted_color = colors[Math.floor(Math.random() * (colors.length))]
+		document.getElementsByTagName('body')[0].style.backgroundColor = "#" + sorted_color;
+
+	} else if (options[1] == true){
+		sorted_color = colors[cS];
+
+	} else if (options[2] == true){
+		sorted_color = document.getElementById("color_custom").value;
+	}
+
+	if (options[3] == true){
+		document.getElementById("text").innerHTML = frases[Math.floor(Math.random() * (frases.length))];
+
+	} else if (options[4] == true){
+		document.getElementById("text").innerHTML = document.getElementById("cT").value;
+	} else if (options[5] == true){
+		document.getElementById("text").innerHTML = document.getElementById("frase_custom").value;
+	}
+
+}
+
+function change_solid(){
+	cS += 1;
+	document.getElementById("cS").style.backgroundColor = "#" + colors[cS];
+	sorted_color = colors[cS];
+
+	if (cS == colors.length - 1){
+		cS = -1;
+	}
+}
+
+function change_text(){
+	cT += 1;
+	document.getElementById("cT").value = frases[cT];
+
+	if (cT == frases.length - 1){
+		cT = -1;
+	}
+}
+
+
+function check(num){
+
+	if (num < 3){
+		for (c = 0; c < 3; c++){options[c] = false}
+		options[num] = true;
+
+		for (c = 0; c < 3; c++){
+			document.getElementsByClassName("checkboxS")[c].checked = false;
+		}
+	} else{
+		for (c = 3; c < 6; c++){options[c] = false}
+		options[num] = true;
+
+		for (c = 3; c < 6; c++){
+			document.getElementsByClassName("checkboxS")[c].checked = false;
+		}
+	}
+
+    document.getElementsByClassName("checkboxS")[num].checked = true;
+}
+
+
+function open_menu(){
+	document.getElementsByTagName('body')[0].style.backgroundColor = "#1F363D";
+	document.getElementById('menu').style.display = "inherit";
+}
+
+function close_menu(){
+	document.getElementById('menu').style.display = "none";
+
+	if (options[2] == true){
+		document.getElementsByTagName('body')[0].style.backgroundColor = "#" + document.getElementById("color_custom").value;;
+	} else{
+		document.getElementsByTagName('body')[0].style.backgroundColor = "#" + sorted_color;
+	}
+}
